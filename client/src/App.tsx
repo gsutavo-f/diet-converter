ddd09913c6a5abd9e4096fa023e5f6f977b59a3f
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-07-02 16:02:27 -0300

    [feat] method getFoodById

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,9 +7,24 @@ interface FoodSelect {
    label: String
 }
 
+interface Food {
+   _id: String,
+   name: String,
+   description: String,
+   calories: Number,
+   protein: Number,
+   carbohydrate: Number
+}
+
 export default function App() {
    const [selectList, setSelectList] = useState<FoodSelect[]>([]);
+   const [selectedFood, setSelectedFood] = useState<Food>();
 
+   function findFoodById(id: String) {
+      Axios.get(`http://localhost:3001/food/${id}`).then((response) => {
+         setSelectedFood(response.data);
+      });
+   }
 
    useEffect(() => {
       Axios.get('http://localhost:3001/food/select').then((response) => {
@@ -22,7 +37,11 @@ export default function App() {
          <Select
             name="food"
             placeholder="Selecione um alimento"
+            value={selectList.find(obj => obj.value === selectedFood?._id)}
             options={selectList}
+            onChange={(event) => {
+               findFoodById(event!.value.toString());
+            }}
          />
       </div>
    );
