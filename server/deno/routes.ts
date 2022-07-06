dbd629d845752a6b47c787e2e62dc5788b16303f
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-07-06 01:54:07 -0300

    [feat] at list something is working :´)

--- server/deno/routes.ts
+++ server/deno/routes.ts
@@ -4,6 +4,13 @@ import FoodController from "./controllers/foodController.ts";
 const router = new Router();
 
 router
+   .get('/', ({ response }: { response: any }) => {
+      response.status = 200;
+      response.body = {
+         success: true,
+         msg: 'This is your server :)',
+      };
+   })
    .get('/food/select', FoodController.getFoodsForSelect)
    .get('/food/:id', FoodController.getFoodById)
    .get('/food', FoodController.getFoods);
