3ec7557e57cdadbc649581ed99e2eafdefe10097
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-07-06 22:43:47 -0300

    [fix] now it's possible to read environment variables

--- server/node/src/config/db.js
+++ server/node/src/config/db.js
@@ -1,4 +1,6 @@
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@greendiet.rm4uq.mongodb.net/green-diet`);
 
