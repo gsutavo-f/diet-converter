ec2afbbed491657f8489e00d08686c317ddff826
gsutavo-f <gustavoperesfernandes@gmail.com>
2022-07-16 20:08:36 -0300

    [feat] added the possibility to add multiple input foods

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,21 +16,26 @@ interface Food {
    carbohydrate: Number
 }
 
+interface FoodGrams {
+   food: Food,
+   grams: Number
+}
+
 export default function App() {
    const [selectList, setSelectList] = useState<FoodSelect[]>([]);
    const [inputFood, setInputFood] = useState<Food>();
    const [outputFood, setOutputFood] = useState<Food>();
-   const [inputGrams, setInputGrams] = useState(100);
+   const [selectedFoods, setSelectedFoods] = useState<FoodGrams[]>([]);
    const [calories, setCalories] = useState(0);
    const [outputGrams, setOutputGrams] = useState(0);
 
-   function findOriginalFoodById(id: String) {
+   function findInputFoodById(id: String) {
       Axios.get(`http://localhost:3001/food/${id}`).then((response) => {
          setInputFood(response.data);
       });
    }
 
-   function findAlternativeFoodById(id: String) {
+   function findOutputFoodById(id: String) {
       Axios.get(`http://localhost:3001/food/${id}`).then((response) => {
          setOutputFood(response.data);
       });
@@ -43,49 +48,86 @@ export default function App() {
    }, []);
 
    useEffect(() => {
-      if (inputFood) {
-         setCalories(Number(inputFood.calories) * inputGrams / 100);
-      }
-   }, [inputFood, inputGrams]);
+      let calculatedCalories = 0;
+      selectedFoods.forEach((item) => {
+         calculatedCalories += (Number(item.food.calories) * Number(item.grams) / 100);
+      });
+      setCalories(Math.round((calculatedCalories + Number.EPSILON) * 100) / 100);
+   }, [selectedFoods]);
 
    useEffect(() => {
       const calculatedGrams = Math.round(((calories / Number(outputFood?.calories) * 100) + Number.EPSILON) * 100) / 100;
       setOutputGrams(Number.isNaN(calculatedGrams) ? 0 : calculatedGrams);
    }, [calories, outputFood]);
 
+   function addSelectedFood() {
+      if (inputFood != null) {
+         setSelectedFoods([...selectedFoods, {food: inputFood, grams: 100}]);
+      }
+   }
+
    return (
-      <div className="md:container md:mx-auto gap-x-40 flex justify-center h-screen items-start">
+      <div className="container mx-auto gap-x-20 flex justify-center h-screen items-start">
          <div className="flex flex-col mt-60">
             <label className="font-semibold" htmlFor="original-food">
                Alimento
             </label>
-            <Select
-               className="w-96"
-               name="original-food"
-               placeholder="Selecione um alimento"
-               value={selectList.find(obj => obj.value === inputFood?._id)}
-               options={selectList}
-               onChange={(event) => {
-                  findOriginalFoodById(event!.value.toString());
-               }}
-            />
-            <label className="mt-3 font-semibold" htmlFor="inputGrams">
-               Gramas
-            </label>
-            <input
-               className="shadow-md p-1 rounded-md"
-               type="number"
-               name="inputGrams"
-               value={inputGrams}
-               onChange={(event) => setInputGrams(event.target.valueAsNumber)}
-               required
-            />
+            <div className="flex flex-row w-96 justify-between">
+               <Select
+                  className="w-80 shadow-lg"
+                  name="original-food"
+                  placeholder="Selecione um alimento"
+                  value={selectList.find(obj => obj.value === inputFood?._id)}
+                  options={selectList}
+                  onChange={(event) => {
+                     findInputFoodById(event!.value.toString());
+                  }}
+               />
+               <button
+                  className="w-12 border border-solid border-gray-300 rounded shadow-lg"
+                  onClick={() => {
+                     addSelectedFood();
+                  }}
+               >
+                  +
+               </button>
+            </div>
+            {selectedFoods.map((item, index) => (
+               <>
+                  <label className="mt-3 font-semibold">
+                     Gramas
+                  </label>
+                  <div className="flex flex-row w-96 justify-between">
+                     <input
+                        className="shadow-lg p-1 rounded-md w-72"
+                        type="string"
+                        value={item.food.name}
+                        disabled
+                     />
+                     <input
+                        className="shadow-lg p-1 rounded-md w-20"
+                        type="number"
+                        value={selectedFoods[index].grams}
+                        onChange={(event) => {
+                           let clone = [...selectedFoods];
+                           let item = {...clone[index]};
+                           item.grams = event.target.valueAsNumber;
+                           clone[index] = item;
+                           setSelectedFoods(clone);
+                        }}
+                        required
+                     />
+                  </div>
+               </>
+            ))}
+         </div>
+         <div className="flex flex-col mt-72">
             <label className="mt-3 font-semibold" htmlFor="calories">
                Calorias
             </label>
             <input
-               className="shadow-md p-1 rounded-md"
-               type="string"
+               className="shadow-lg p-1 rounded-md"
+               type="text"
                name="calories"
                value={`${calories} kcal`}
                disabled
@@ -96,21 +138,21 @@ export default function App() {
                Alimento
             </label>
             <Select
-               className="w-96"
+               className="w-96 shadow-lg"
                name="alternative-food"
                placeholder="Selecione um alimento"
                value={selectList.find(obj => obj.value === outputFood?._id)}
                options={selectList}
                onChange={(event) => {
-                  findAlternativeFoodById(event!.value.toString());
+                  findOutputFoodById(event!.value.toString());
                }}
             />
             <label className="mt-3 font-semibold" htmlFor="outputGrams">
                Gramas
             </label>
             <input
-               className="shadow-md p-1 rounded-md"
-               type="string"
+               className="shadow-lg p-1 rounded-md"
+               type="text"
                name="outputGrams"
                value={`${outputGrams} g`}
                disabled
